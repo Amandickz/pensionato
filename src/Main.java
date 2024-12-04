82d8e796da310d9c8a943014112d49b4e37eed83
Amandickz <amandickz@gmail.com>
2024-12-04 15:11:44 -0300

    Finalização do Exercício

--- src/Main.java
+++ src/Main.java
@@ -10,16 +10,42 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		
 		Aluguel alugueis[] = new Aluguel[10];
-		int n;
+		int n, num_quarto;
+		int cont = 0;
+		String nome, email;
 		
-		System.out.print("Digite o número de estudante que vão alugar quartos: ");
+		System.out.print("Quantos quartos serão alugados? ");
 		n = scan.nextInt();
 		
 		if(n <= 0 || n > 10) {
 			System.out.println("Número inválido");
 		} else {
 			
+			while(cont < n) {
+				System.out.print("\nQuarto: ");
+				num_quarto = scan.nextInt();
+				System.out.print("Nome do estudante: ");
+				scan.nextLine();
+				nome = scan.nextLine();
+				System.out.print("E-mail: ");
+				email = scan.nextLine();
+				
+				if(alugueis[num_quarto-1] == null) {
+					alugueis[num_quarto-1] = new Aluguel(nome, email);
+					cont++;
+				}
+				
+			}
 			
+			System.out.println("\n");
+			
+			for(int i = 0; i < 10; i++) {
+				
+				if(alugueis[i] != null) {
+					System.out.println("Quarto #" + (i+1) + ": " + alugueis[i].toString());
+				}
+				
+			}
 			
 		}
 		
